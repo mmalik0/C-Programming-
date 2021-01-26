677a1752ab6cfa325ace57c654303758fd5e3eb8
Muhammad Malik <mafnanmalik@hotmail.com>
2021-01-26 00:27:26 -0500

    added pointer arithmetic

--- pointers/basicPointers.c
+++ pointers/basicPointers.c
@@ -15,13 +15,16 @@ int main()
     a=5; //initialized 'a'
     p= &a; //assigns address of 'a' to pointer 'p'
 
-    printf("value of a is %d\n",a);
     printf("address of a is %d\n",&a);
+    printf("value of a is %d\n",a);
 
-    printf("value of a passed by derefrencing to p is %d\n", *p);
     printf("address of a stored in p is %d\n",p);
+    printf("value of a passed by derefrencing to p is %d\n", *p);
 
-    
+    *p = 21;
+
+    printf("address of a stored in p after modification is %d\n",p);
+    printf("value of \'a\' modified through pointer is %d\n", *p);
 
     return 0;
 }
