ecac525163a721f6b544ae5ece703988ea56ed81
Muhammad Malik <mafnanmalik@hotmail.com>
2021-01-26 00:20:46 -0500

    added comments

--- pointers/basicPointers.c
+++ pointers/basicPointers.c
@@ -9,17 +9,19 @@
 
 int main()
 {
-    int a;
+    int a; 
     int* p;
 
-    a=5;
-    p= &a;
+    a=5; //initialized 'a'
+    p= &a; //assigns address of 'a' to pointer 'p'
 
     printf("value of a is %d\n",a);
     printf("address of a is %d\n",&a);
 
     printf("value of a passed by derefrencing to p is %d\n", *p);
     printf("address of a stored in p is %d\n",p);
+
+    
 
     return 0;
 }
