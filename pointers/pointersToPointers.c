99d154df21d89d1d83da2bbfd481084088b7ddb7
Muhammad Malik <mafnanmalik@hotmail.com>
2021-01-29 00:56:54 -0500

    initialized

---
+++ pointers/pointersToPointers.c
@@ -0,0 +1,18 @@
+/********************
+ * Muhammad Malik
+ * Pointers to pointers
+ * 29th January, 2021
+ * ******************/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+    int x; 
+    int* y = &x; 
+    int** z = &y;
+    int*** i = &z;
+
+return 0;
+}
